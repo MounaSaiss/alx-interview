410c672a4bab68c8c3b4f854f14e53b6edf53f45
MounaSaiss <mouna1.saiss@gmail.com>
2025-01-29 20:25:58 +0000

    first

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,31 +1,28 @@
-def isWinner(x, nums):
-    # Function to find all prime numbers up to n using the Sieve of Eratosthenes
-    def sieve(n):
-        is_prime = [True] * (n + 1)
-        is_prime[0] = is_prime[1] = False
-        for i in range(2, int(n**0.5) + 1):
-            if is_prime[i]:
-                for j in range(i * i, n + 1, i):
-                    is_prime[j] = False
-        return [i for i in range(2, n + 1) if is_prime[i]]
-    
-    maria_wins = 0
-    ben_wins = 0
-    
-    # Simulate each round
-    for n in nums:
-        primes = sieve(n)
-        # If the number of primes is odd, Maria wins, else Ben wins
-        if len(primes) % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-    
-    # Return the player who won more rounds
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+#!/usr/bin/python3
+'''Prime game module.
+'''
 
+
+def isWinner(x, nums):
+    '''Determines the winner from an array of game played
+    '''
+    if x < 1 or not nums:
+        return None
+    maria, ben = 0, 0
+
+    max_n = max(nums)
+    primes = [True for _ in range(1, max_n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, max_n + 1, i):
+            primes[j - 1] = False
+
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        ben += primes_count % 2 == 0
+        maria += primes_count % 2 == 1
+    if maria == ben:
+        return None
+    return 'Maria' if maria > ben else 'Ben'
